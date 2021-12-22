a9461218dd14fe692ed67f552c1e0a12ec346cda
sabilid <75723653+sabilid@users.noreply.github.com>
2021-12-22 15:50:50 +0000

    Update _footer.php

--- _footer.php
+++ _footer.php
@@ -1,7 +1,7 @@
 <section id="footer_main" class="clearfix">
     <div class="col-sm-12">
         <div class="footer_main_1">
-            <p>© 2021 Your Website Name. All Rights Reserved. Design by Loutf-C KRYSYLY<a href="http://www.TemplateOnWeb.com"> Template On Web</a></p>
+            <p>© 2021 Your Website Name. All Rights Reserved. Design by Loutf-C KRYSYLY</p>
         </div>
     </div>
 </section>
